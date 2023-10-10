69a913dd2c58598a53a226fd410ed96da55f5b93
Homo-Faber-Fa <133135868+Homo-Faber-Fa@users.noreply.github.com>
2023-10-10 17:55:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+
 public class Main {
+
+    public static final Map<Integer, Integer> SIZWE_TO_FREQ = new HashMap<>();
+
     public static void main(String[] args) {
-        final Map<Integer, Integer> SIZWE_TO_FREQ = new HashMap<>();
+
+
         for (int i = 0; i < 1000; i++) {
             int numberOfThread = i + 1;
             new Thread(() -> {
                 String result = generateRoute("RLRFR", 100);
                 int resultLength = result.replaceAll("[^R]", "").length();
+                //int resultLength = (int) route.chars().filter(ch -> ch == 'R').length();
+
                 synchronized (SIZWE_TO_FREQ) {
                     if (SIZWE_TO_FREQ.containsKey(resultLength)) {
                         SIZWE_TO_FREQ.put(resultLength, SIZWE_TO_FREQ.get(resultLength) + 1);
@@ -17,11 +24,23 @@ public class Main {
                         SIZWE_TO_FREQ.put(resultLength, 1);
                     }
                 }
-                System.out.println("Поток №" + numberOfThread + " число R - " + resultLength);
+                System.out.println("- " + numberOfThread + " число R - (" + resultLength + " раз)");//
+
             }).start();
+
         }
+
+        Map.Entry<Integer, Integer> max = SIZWE_TO_FREQ
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get();
+
+        System.out.println("Самое частое количество повторений  " + max.getKey()
+                + "(встретилось  " + max.getValue() + " раз)");
     }
 
+    //Для генерации маршрутов вы используете функцию:
     public static String generateRoute(String letters, int length) {
         Random random = new Random();
         StringBuilder route = new StringBuilder();
